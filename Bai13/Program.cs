f69fd9df64a39f4cd63e759017ac9efe4316aa77
Minh Hoàng <51706032+TLMHoang@users.noreply.github.com>
2019-11-25 21:34:08 +0700

    Bài 13 chưa hoàn thành

--- Bai13/Program.cs
+++ Bai13/Program.cs
@@ -8,8 +8,99 @@ namespace Bai13
 {
     class Program
     {
+        //Nhập vào 1 chuỗi, sau đó:
+        //1. Đếm xem chuỗi có bao nhiêu in HOA, in thường, số
+        //2. Đếm số khoảng trắng trong chuỗi.
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            BaiTap1();
+
+           
         }
+
+        static void BaiTap1()
+        {
+            Console.Clear();
+            Console.WriteLine("Câu 1: Đếm xem chuỗi có bao nhiêu in HOA, in thường, số\n\n");
+            Console.Write("Nhập Chuỗi:");
+            string s = Console.ReadLine();
+            int DemInHoa = 0, DemInThuong = 0, DemSo = 0, DemKT = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsLower(s[i]))
+                {
+                    DemInThuong++;
+                    continue;
+                }
+                    
+                if (char.IsUpper(s[i]))
+                {
+                    DemInHoa++;
+                    continue;
+                }
+                if (char.IsDigit(s[i]))
+                {
+                    DemSo++;
+                    continue;
+                }
+                if (char.IsWhiteSpace(s[i]))
+                {
+                    DemKT++;
+                    continue;
+                }
+            }
+            Console.WriteLine("Có {0} ký tự in thường", DemInThuong);
+            Console.WriteLine("Có {0} ký tự in HOA", DemInHoa);
+            Console.WriteLine("Có {0} ký tự số", DemSo);
+            Console.WriteLine("Có {0} ký tự khoảng trắng", DemKT);
+            Console.WriteLine("Bấm phím bất kỳ để tiếp tục");
+            Console.ReadKey();
+        }
+
+        static void FindString(string VString, string Find)
+        {
+            Console.Clear();
+            Console.WriteLine("Câu 2: Đếm số khoảng trắng (Ký tự) trong chuỗi\n\n");
+            Console.Write("Nhập Chuỗi:");
+            string s = Console.ReadLine();
+
+            string Chinh = VString.ToUpper();
+            string Phu = Find.ToUpper();
+            if (Chinh.IndexOf(Phu) == -1)
+            {
+                Console.WriteLine("\"" + Find + "\" Không xuất hiện trong \"" + VString + "\"");
+            }
+            else
+            {
+                int StartS = Chinh.IndexOf(Phu);
+                Console.WriteLine(VString.Substring(StartS, Find.Length) + " tại vị trí {0} đến {1} \n", StartS, StartS + Find.Length - 1);
+            }
+        }
+
+        static void HandleString3()
+        {
+            Console.Write("Nhập Chuỗi 1:");
+            string s1 = Console.ReadLine();
+
+            Console.WriteLine("Nhập Chuỗi 2:");
+            string s2 = Console.ReadLine();
+
+            int kq = s1.ToLower().CompareTo(s2.ToLower());
+            if (kq == 0)
+            {
+                Console.WriteLine("s và s2 giống nhau");
+            }
+            else if (kq < 0)
+            {
+                Console.WriteLine("s < s2");
+            }
+            else
+            {
+                Console.WriteLine("s > s2");
+            }
+            Console.ReadLine();
+        }
+
     }
 }
